9afaf3df2e4c62b3b002cd8359e8b7201fb3c151
cuong3004 <duongduccuong30042002@gmail.com>
2022-02-12 19:50:49 +0700

    update

--- scripts/export_csv.py
+++ scripts/export_csv.py
@@ -10,7 +10,7 @@ def export(link_file, output_file):
     with open(link_file, 'r') as json_file:
         json_list = list(json_file)
     
-    df = pd.DataFrame()
+    df = pd.DataFrame().iloc[:100]
 
     list_id = []
     list_sentence = []
